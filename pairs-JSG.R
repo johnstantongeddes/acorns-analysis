c2c0d94c6e29e6adc159ac228e08898004107182
John Stanton-Geddes <john.stantongeddes.research@gmail.com>
2013-10-01 10:23:14 -0400

    Completed modification of pairs script to include custom values in lower triangle. Included external function (pairs-JSG.R) with this modification to the pairs plot. Not elegant as the code is hard to understand, but functional.

--- pairs-JSG.R
+++ pairs-JSG.R
@@ -1,7 +1,7 @@
 pairsJSG <- function (x, labels, panel = points, ..., lower.panel = panel, 
     upper.panel = panel, diag.panel = NULL, text.panel = textPanel, 
     label.pos = 0.5 + has.diag/3, line.main = 3, cex.labels = NULL, 
-    font.labels = 1, row1attop = TRUE, gap = 1, log = "") 
+    font.labels = 1, row1attop = TRUE, gap = 1, log = "", m = m) 
 {
     if (doText <- missing(text.panel) || is.function(text.panel)) 
         textPanel <- function(x = 0.5, y = 0.5, txt, cex, font) text(x, 
@@ -118,8 +118,9 @@ pairsJSG <- function (x, labels, panel = points, ..., lower.panel = panel,
                 }
             }
             else if (i < j) 
-                localLowerPanel(as.vector(x[,j]), as.vector(x[,i]), ...)
-            else localUpperPanel(j, i, ...)
+                localLowerPanel(as.vector(x[, j]), as.vector(x[, 
+                  i]), ...)
+            else localUpperPanel(i, j, m, ...)
             if (any(par("mfg") != mfg)) 
                 stop("the 'panel' function made a new plot")
         }
@@ -142,6 +143,7 @@ pairsJSG <- function (x, labels, panel = points, ..., lower.panel = panel,
 panel.sm <- function(x, y, col = par("col"), bg = NA, pch = par("pch"), cex = 1, col.smooth = "red", span = 2/3, iter = 3, ...) 
 {
     points(x, y, pch = pch, col = col, bg = bg, cex = cex)
+    
     ok <- is.finite(x) & is.finite(y)
     if (any(ok)) 
         lines(stats::lowess(x[ok], y[ok], f = span, iter = iter), 
@@ -149,11 +151,11 @@ panel.sm <- function(x, y, col = par("col"), bg = NA, pch = par("pch"), cex = 1,
 }
 
 
-panel.sp <- function(x, y, digits=2, prefix="", cex.cor, ...)
+panel.sp <- function(x, y, m, digits=2, prefix="", cex.cor, ...)
 {
      usr <- par("usr"); on.exit(par(usr))
      par(usr = c(0, 1, 0, 1))
-     txt <- x+y
+     txt <- m[x,y]
      text(0.5, 0.5, txt)
 }
 
